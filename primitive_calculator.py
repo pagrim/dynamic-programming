b04c66d75ceca6e0214d5acb9c93d0549b824ac8
Peter Grimshaw <pagrimshaw@gmail.com>
2021-10-11 09:34:08 +0100

    Refactored to single variable for current value, tests passing

--- primitive_calculator.py
+++ primitive_calculator.py
@@ -4,29 +4,35 @@ from operator import itemgetter
 
 class PrimitiveCalculator:
 
-    def __init__(self, operations):
+    def __init__(self, operations, validators):
         self.operations = operations
+        self.validators = validators
         self.memo = {}
 
-    def find_min_operations(self, target: int) -> int:
-        min_operations_seq = self._find_min_operations([], 1, target)
-        return len(min_operations_seq)
+    def clear_memo(self):
+        self.memo = {}
 
-    def _find_min_operations(self, sequence: List[int], curr: int, target: int) -> List[int]:
-        print('Seq:', sequence, 'Current:', curr, 'Target:', target)
-        if curr == target:
-            return sequence + [curr]
+    def find_min_operations(self, target: int) -> List[int]:
+        return self._find_min_operations([], target)
+
+    def count_min_operations(self, target: int):
+        return len(self.find_min_operations(target))
+
+    def _find_min_operations(self, sequence: List[int], target: int) -> List[int]:
+        print('Seq:', sequence, 'Target:', target)
+        if target == 1:
+            return [target] + sequence
         else:
             try:
-                min_ops = self.memo[curr]
+                seq_result = self.memo[target]
             except KeyError:
-                valid_operations = [i for i, op in enumerate(self.operations) if op(curr) <= target]
+                valid_operations = [i for i, vld in enumerate(self.validators) if vld(target)]
                 print('Valid operations:', valid_operations)
-                sequence_options = [self._find_min_operations(sequence + [curr], self.operations[vo](curr), target) for vo in valid_operations]
+                sequence_options = [self._find_min_operations([target] + sequence, self.operations[vo](target)) for vo in valid_operations]
                 print('Sequence options:', sequence_options)
                 min_seq = min([(i, len(so)) for i, so in enumerate(sequence_options)], key=itemgetter(1))[0]
                 print('Minimum seq idx:', min_seq)
-                min_ops = sequence_options[min_seq]
-                print('Min ops:', min_ops)
-                self.memo = min_ops
-            return min_ops
+                seq_result = sequence_options[min_seq]
+                print('Min operations:', seq_result)
+                self.memo[target] = seq_result
+            return seq_result
--- test_primitive_calculator.py
+++ test_primitive_calculator.py
@@ -1,9 +1,23 @@
 from primitive_calculator import PrimitiveCalculator
 
-pc = PrimitiveCalculator([lambda x: x*2, lambda x: x*3, lambda x: x+1])
+pc = PrimitiveCalculator([lambda x: x//2, lambda x: x//3, lambda x: x-1], [lambda x: x % 2 == 0, lambda x: x % 3 == 0, lambda x: x > 1])
 
 
-def test_find_min_operations_base():
-    assert (pc._find_min_operations([[1, 2, 3], [1, 3]], 2, 3) == [1, 3])
-    #assert(pc._find_min_operations([], 1, 1) == [1])
-    #assert(pc._find_min_operations([], 1, 3) == [1, 3])
+def test__find_min_operations_base():
+    assert(pc._find_min_operations([3], 1) == [1, 3])
+    assert (pc._find_min_operations([], 1) == [1])
+    pc.clear_memo()
+
+
+def test__find_min_operations():
+    assert(pc._find_min_operations([6], 3) == [1, 3, 6])
+    pc.clear_memo()
+
+
+def test_find_min_operations_9():
+    assert(pc.find_min_operations(9) == [1, 3, 9])
+    pc.clear_memo()
+
+def test_find_min_operations_8():
+    assert(pc.find_min_operations(8) == [1, 2, 4, 8])
+    pc.clear_memo()
