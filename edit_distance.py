b5d84079b65a1bb9742ee164b72a9c7bfb14e500
Peter Grimshaw <Peter.Grimshaw@gfk.com>
2021-10-12 18:27:36 +0100

    Fixed edit distance, added tests, tests passing

--- edit_distance.py
+++ edit_distance.py
@@ -3,20 +3,26 @@ import numpy as np
 class EditDistance:
 
     def __init__(self, word1, word2):
-        self.word1 = word1
-        self.word2 = word2
-        self.memo = np.ones((len(self.word1), len(self.word2))) * -1
+        self.spacer = "-"
+        self.word1 = self.spacer + word1
+        self.word2 = self.spacer + word2
+        self.memo = self.initialise_memo()
+
+    def initialise_memo(self):
+        init_memo = np.ones((len(self.word1), len(self.word2))) * -1
+        init_memo[0, 0] = 0
+        return init_memo
 
     def calculate(self):
-        return _calculate(self, len(word1) -1, len(word2) -1)
+        return self._calculate(len(self.word1) -1, len(self.word2) -1)
 
     def _calculate(self, i, j):
         print('_calculate with:', i, j)
         if i == 0 and j == 0:
-            return 0
+            edit_distance = 0
         else:
             if self.memo[i, j] != -1:
-                return self.memo[i, j]
+                edit_distance = self.memo[i, j]
             else:
                 substitution_cost = 0 if self.word1[i] == self.word2[j] else 1
                 next_indices = [(i-1, j), (i, j-1), (i-1, j-1)]
@@ -26,4 +32,7 @@ class EditDistance:
                 print('Next costs:', next_costs)
                 print('Next indices valid:', next_indices_valid)
                 self.memo[i, j] = min([self._calculate(*ni) + nc for ni, nc, niv in zip(next_indices, next_costs, next_indices_valid) if niv])
-                return self.memo[i, j]
+                print('Set distance for (i, j)', i, j, 'to', self.memo[i, j])
+                edit_distance = self.memo[i, j]
+        print('Updated memo', self.memo)
+        return edit_distance
--- test_edit_distance.py
+++ test_edit_distance.py
@@ -1,6 +1,10 @@
 from edit_distance import EditDistance
 
-ed = EditDistance('app', 'happen')
-
 def test__calculate():
-    assert(ed._calculate(2, 2) == 2)
+    ed1 = EditDistance('app', 'happen')
+    assert(ed1._calculate(2, 2) == 2)
+
+def test_calculate():
+    assert(EditDistance('ab', 'ab').calculate() == 0)
+    assert(EditDistance('short', 'ports').calculate() == 3)
+    assert(EditDistance('editing', 'distance').calculate() == 5)
