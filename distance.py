fc22cabab3c914343bbbe6151fd1fc3881bb7afb
Peter Grimshaw <pagrimshaw@gmail.com>
2021-10-24 16:22:43 +0100

    Adapted EditDistance code for n dimensions, tests passing

--- distance.py
+++ distance.py
@@ -1,67 +1,81 @@
 import numpy as np
+from itertools import product, groupby
+from operator import mul
+from functools import reduce
+
 
 class Distance:
 
-    def __init__(self, word1, word2):
+    def __init__(self, words):
         self.spacer = "-"
-        self.word1 = self.spacer + word1
-        self.word2 = self.spacer + word2
+        self.words = [self.spacer + word for word in words]
         self.memo = self.initialise_memo()
 
     def initialise_memo(self):
-        init_memo = np.ones((len(self.word1), len(self.word2))) * -1
-        init_memo[0, 0] = 0
+        word_lengths = [len(word) for word in self.words]
+        memo_size = reduce(mul, word_lengths)
+        init_values = np.ones(memo_size) * -1
+        init_values[0] = 0
+        init_memo = np.reshape(init_values, word_lengths)
         return init_memo
 
     def calculate(self):
-        return self._calculate(len(self.word1) -1, len(self.word2) -1)
+        return self._calculate([len(word) -1 for word in self.words])
 
-    def _calculate(self, i, j):
-        print('_calculate with:', i, j)
-        if i == 0 and j == 0:
+    def _calculate(self, index):
+        print('_calculate with:', index)
+        if sum(index) == 0:
             distance = 0
         else:
-            if self.memo[i, j] != -1:
-                distance = self.memo[i, j]
+            memo_value = self.memo[tuple(index)]
+            if memo_value != -1:
+                distance = memo_value
             else:
-                self.memo[i, j] = self.get_min_max(i, j)
-                print('Set distance for (i, j)', i, j, 'to', self.memo[i, j])
-                distance = self.memo[i, j]
+                computed_value = self.get_min_max(index)
+                self.memo[tuple(index)] = computed_value
+                print('Set distance for', index, 'to', computed_value)
+                distance = computed_value
         print('Updated memo', self.memo)
         return distance
 
     @staticmethod
     def check_valid(next_indices):
-        next_indices_valid = [ni[0] >= 0 and ni[1] >= 0 for ni in next_indices]
+        next_indices_valid = ([all([ni >= 0 for ni in nxt_idcs]) for nxt_idcs in next_indices])
         print('Next indices valid:', next_indices_valid)
         return next_indices_valid
 
     @staticmethod
-    def get_next_indices(i, j):
-        next_indices = [(i-1, j), (i, j-1), (i-1, j-1)]
+    def get_next_indices(index):
+        index_ranges = [[idx, idx -1] for idx in index]
+        next_indices = [list(idx) for idx in product(*index_ranges) if list(idx) != index]
         print('Next indices:', next_indices)
         return next_indices
 
-    def get_min_max(self, i, j):
+    def get_min_max(self, index):
         pass
 
 
 class EditDistance(Distance):
 
-    def __init__(self, word1, word2):
-        super().__init__(word1, word2)
+    def __init__(self, words):
+        super().__init__(words)
 
-    def get_next_costs(self, i, j):
-        substitution_cost = 0 if self.word1[i] == self.word2[j] else 1
-        next_costs = [1, 1, substitution_cost]
+    def get_next_costs(self, index, next_indices):
+        next_costs = [self.get_substitution_cost(index) if (sum(index) - sum(ni)) > 1 else 1 for ni in next_indices]
         print('Next costs:', next_costs)
         return next_costs
 
-    def get_min_max(self, i, j):
-        next_indices = self.get_next_indices(i, j)
-        next_costs = self.get_next_costs(i, j)
+    def get_substitution_cost(self, index):
+        next_chars = [wrd[ni] for wrd, ni in zip(self.words, index)]
+        grouped_next_chars = groupby(next_chars)
+        substitution_cost = 1 if next(grouped_next_chars, True) and next(grouped_next_chars, False) else 0
+        return substitution_cost
+
+    def get_min_max(self, index):
+        next_indices = self.get_next_indices(index)
+        next_costs = self.get_next_costs(index, next_indices)
         next_indices_valid = self.check_valid(next_indices)
-        return min([self._calculate(*ni) + nc for ni, nc, niv in zip(next_indices, next_costs, next_indices_valid) if niv])
+        return min([self._calculate(ni) + nc for ni, nc, niv in zip(next_indices, next_costs, next_indices_valid) if niv])
 
 
 class LongestCommonSubsequence(Distance):
--- test_distance.py
+++ test_distance.py
@@ -1,18 +1,54 @@
 from distance import EditDistance, LongestCommonSubsequence
 
 
-def test_ED__calculate():
-    ed1 = EditDistance('app', 'happen')
-    assert(ed1._calculate(2, 2) == 2)
+def test_ed__calculate_1():
+    ed1 = EditDistance(['app', 'happen'])
+    assert (ed1._calculate([2, 2]) == 2)
 
 
-def test_ED_calculate():
-    assert(EditDistance('ab', 'ab').calculate() == 0)
-    assert(EditDistance('short', 'ports').calculate() == 3)
-    assert(EditDistance('editing', 'distance').calculate() == 5)
+def test_ed__calculate_2():
+    ed1 = EditDistance(['s', 'p'])
+    assert (ed1._calculate([1, 1]) == 1)
 
 
-def test_LCS_calculate():
-    assert(LongestCommonSubsequence('275', '25').calculate() == 2)
-    assert(LongestCommonSubsequence('7', '1234').calculate() == 0)
-    assert(LongestCommonSubsequence('2783', '5287').calculate() == 2)
+def test_ed_substitution_cost_1():
+    ed1 = EditDistance(['app', 'happen'])
+    assert (ed1.get_substitution_cost([1, 1]) == 1)
+
+
+def test_ed_substitution_cost_2():
+    ed1 = EditDistance(['ports', 'short'])
+    assert (ed1.get_substitution_cost([1, 1]) == 1)
+
+
+def test_ed_get_next_costs_1():
+    ed1 = EditDistance(['ab', 'ab'])
+    assert (ed1.get_next_costs([2, 2], [[2, 1], [1, 2], [1, 1]]) == [1, 1, 0])
+
+
+def test_ed_get_next_costs_2():
+    ed1 = EditDistance(['app', 'happen'])
+    assert (ed1.get_next_costs([2, 2], [[2, 1], [1, 2], [1, 1]]) == [1, 1, 1])
+
+
+def test_ed_get_next_costs_3():
+    ed1 = EditDistance(['ports', 'short'])
+    assert (ed1.get_next_costs([2, 2], [[2, 1], [1, 2], [1, 1]]) == [1, 1, 1])
+
+
+def test_ed_calculate_1():
+    assert (EditDistance(['ab', 'ab']).calculate() == 0)
+
+
+def test_ed_calculate_2():
+    assert (EditDistance(['short', 'ports']).calculate() == 3)
+
+
+def test_ed_calculate_3():
+    assert (EditDistance(['editing', 'distance']).calculate() == 5)
+
+
+def test_lcs_calculate():
+    assert (LongestCommonSubsequence('275', '25').calculate() == 2)
+    assert (LongestCommonSubsequence('7', '1234').calculate() == 0)
+    assert (LongestCommonSubsequence('2783', '5287').calculate() == 2)
