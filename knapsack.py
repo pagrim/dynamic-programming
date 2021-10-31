ec8f4fd0bf3bad63683978ce9c1f6614ed917632
Peter Grimshaw <pagrimshaw@gmail.com>
2021-10-31 10:56:35 +0000

    Added weights used and no repetitions case

--- knapsack.py
+++ knapsack.py
@@ -21,18 +21,19 @@ class Knapsack:
         self.memo = [None for _ in range(capacity + 1)]
 
     def calc_max_value(self):
-        max_value = self._calc_max_value(self.capacity)
+        max_value = self._calc_max_value(self.capacity, [0 for _ in self.weights])
         logger.info('Memo: %s', self.memo)
         return max_value
 
-    def _calc_max_value(self, target_weight):
+    def _calc_max_value(self, target_weight, weights_used):
         if target_weight == 0:
+            logger.info('Found zero option, weights used %s', weights_used)
             max_val = 0
         else:
-            option_weights_values = self.fetch_options(target_weight)
-            logger.info('Target weight %s, options weights, values %s', target_weight, option_weights_values)
-            options = [self._calc_max_value(opt_wgt) + val for opt_wgt, val in option_weights_values]
-            logger.info('Target weight %s, options %s', target_weight, options)
+            option_triples = self.fetch_options(target_weight, weights_used)
+            logger.info('Target weight %s, options weights, values, indexes %s', target_weight, option_triples)
+            options = [self._calc_max_value(opt_wgt, self.update_weights(weights_used, idx)) + val for opt_wgt, val, idx in option_triples]
+            logger.info('Target weight %s, options %s, weights used %s', target_weight, options, weights_used)
             if len(options) == 0:
                 max_val = 0
             else:
@@ -40,8 +41,12 @@ class Knapsack:
             self.memo[target_weight] = max_val
         return max_val
 
-    def fetch_options(self, target_weight):
-        return [(target_weight - wgt, val) for wgt, val in self.weights_values if self.valid_weight(wgt, target_weight)]
+    def fetch_options(self, target_weight, weights_used):
+        return [(target_weight - wgt, val, idx) for idx, (wgt, val) in enumerate(self.weights_values) if self.valid_weight(wgt, target_weight) and self.available_weight(weights_used, idx)]
+
+
+    def update_weights(self, weights_used, weight_index):
+        return [wu +1 if i == weight_index else wu for i, wu in enumerate(weights_used)]
 
     def zip_weights_values(self, weights, values):
         assert(len(weights) == len(values))
@@ -62,3 +67,11 @@ class KnapsackWithRepetition(Knapsack):
 
     def available_weight(self, weights_used, index):
         return True
+
+class KnapsackNoRepetition(Knapsack):
+
+    def __init__(self, weights, values, capacity):
+        super().__init__(weights, values, capacity)
+
+    def available_weight(self, weights_used, index):
+        return weights_used[index] == 0
