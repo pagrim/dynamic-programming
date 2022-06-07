79cb486d85d4023271906e42a6d45000e15fa0af
Peter Grimshaw <pagrimshaw@gmail.com>
2022-06-07 18:03:51 +0100

    Replaced print with logging in distance.py

--- distance.py
+++ distance.py
@@ -2,6 +2,7 @@ import numpy as np
 from itertools import product, groupby
 from operator import mul
 from functools import reduce
+import logging
 
 
 class Distance:
@@ -10,6 +11,7 @@ class Distance:
         self.spacer = "-"
         self.words = [[self.spacer] + [char for char in word] for word in words]
         self.memo = self.initialise_memo()
+        self.logger = logging.getLogger('distance')
 
     def initialise_memo(self):
         word_lengths = [len(word) for word in self.words]
@@ -23,7 +25,7 @@ class Distance:
         return self._calculate([len(word) -1 for word in self.words])
 
     def _calculate(self, index):
-        print('_calculate with:', index)
+        self.logger.debug('_calculate with: %s', index)
         if sum(index) == 0:
             distance = 0
         else:
@@ -33,22 +35,20 @@ class Distance:
             else:
                 computed_value = self.get_min_max(index)
                 self.memo[tuple(index)] = computed_value
-                print('Set distance for', index, 'to', computed_value)
+                self.logger.debug('Set distance for %s to %s', index, computed_value)
                 distance = computed_value
-        print('Updated memo', self.memo)
+        self.logger.debug('Updated memo \n%s', self.memo)
         return distance
 
-    @staticmethod
-    def check_valid(next_indices):
+    def check_valid(self, next_indices):
         next_indices_valid = ([all([ni >= 0 for ni in nxt_idcs]) for nxt_idcs in next_indices])
-        print('Next indices valid:', next_indices_valid)
+        self.logger.debug('Next indices valid: %s', next_indices_valid)
         return next_indices_valid
 
-    @staticmethod
-    def get_next_indices(index):
+    def get_next_indices(self, index):
         index_ranges = [[idx, idx -1] for idx in index]
         next_indices = [list(idx) for idx in product(*index_ranges) if list(idx) != index]
-        print('Next indices:', next_indices)
+        self.logger.debug('Next indices: %s', next_indices)
         return next_indices
 
     def get_min_max(self, index):
@@ -71,7 +71,7 @@ class EditDistance(Distance):
 
     def get_next_costs(self, index, next_indices):
         next_costs = [self.get_substitution_cost(index) if (sum(index) - sum(ni)) > 1 else 1 for ni in next_indices]
-        print('Next costs:', next_costs)
+        self.logger.debug('Next costs: %s', next_costs)
         return next_costs
 
     def get_min_max(self, index):
@@ -94,7 +94,7 @@ class LongestCommonSubsequence(Distance):
 
     def get_next_costs(self, index, next_indices):
         next_costs = [self.get_substitution_cost(index) if (sum(index) - sum(ni)) > 1 else 0 for ni in next_indices]
-        print('Next costs:', next_costs)
+        self.logger.debug('Next costs: %s', next_costs)
         return next_costs
 
     def get_min_max(self, index):
