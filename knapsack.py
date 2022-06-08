60690dd339e55ea26d72b2063bc68ff97112d725
Peter Grimshaw <pagrimshaw@gmail.com>
2022-06-08 14:54:33 +0100

    Adjusted knapsack to use memo and iterate through, cleaned code and adjusted tests.

--- knapsack.py
+++ knapsack.py
@@ -1,16 +1,14 @@
 import logging
 import sys
 import re
+from collections import namedtuple
+
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger('knapsack')
 
-
-class WeightsValue:
-
-    def __init__(self, weights, values):
-        self.weights = weights
-        self.values = values
+Option = namedtuple("Option", ["weight", "value", "idx"])
+MemoItem = namedtuple("MemoItem", ["value", "weights_used", "prev_weight"])
 
 
 class Knapsack:
@@ -20,46 +18,39 @@ class Knapsack:
         self.values = values
         self.weights_values = self.zip_weights_values(weights, values)
         self.capacity = capacity
-        self.memo = [None for _ in range(capacity + 1)]
+        self.memo = [MemoItem(-1, [], -1) for _ in range(self.capacity + 1)]
+        self.weights_used = [0 for _ in self.weights]
 
     def calc_max_value(self):
-        max_value = self._calc_max_value(self.capacity, [0 for _ in self.weights])
+        self._calc_max_value()
         logger.info('Memo: %s', self.memo)
-        return max_value
+        return max(self.memo, key=lambda mi: mi.value)
 
-    def _calc_max_value(self, target_weight, weights_used):
-        if target_weight == 0:
-            logger.debug('Found zero option, weights used %s', weights_used)
-            max_val = 0
-        else:
-            option_triples = self.fetch_options(target_weight, weights_used)
-            logger.debug('Target weight %s, options weights, values, indexes %s', target_weight, option_triples)
-            options = [self._calc_max_value(opt_wgt, self.update_weights(weights_used, idx)) + val for opt_wgt, val, idx in option_triples]
-            logger.debug('Target weight %s, options %s, weights used %s', target_weight, options, weights_used)
-            if len(options) == 0:
-                max_val = 0
-            else:
-                max_val = max(options)
-            self.memo[target_weight] = max_val
-        return max_val
+    def _calc_max_value(self):
+        self.memo[0] = MemoItem(0, [0 for _ in self.weights], 0)
+        for trgt in range(1, self.capacity + 1):
+            for idx, (wgt, val) in enumerate(self.weights_values):
+                opt = self.valid_option(idx, wgt, val, trgt)
+                if opt:
+                    opt_value = self.memo[opt.weight].value + opt.value
+                    if opt_value > self.memo[trgt].value:
+                        prev_weights = self.memo[opt.weight].weights_used
+                        new_weights = [wgt + 1 if i == idx else wgt for i, wgt in enumerate(prev_weights)]
+                        self.memo[trgt] = MemoItem(opt_value, new_weights, opt.weight)
 
-    def fetch_options(self, target_weight, weights_used):
-        return [(target_weight - wgt, val, idx) for idx, (wgt, val) in enumerate(self.weights_values) if self.valid_weight(wgt, target_weight) and self.available_weight(weights_used, idx)]
+    def valid_option(self, index, weight, value, target):
+        option_weight = target - weight
+        if option_weight >= 0 and self.available_weight(option_weight, index):
+            return Option(option_weight, value, index)
 
 
-    def update_weights(self, weights_used, weight_index):
-        return [wu +1 if i == weight_index else wu for i, wu in enumerate(weights_used)]
+    @staticmethod
+    def zip_weights_values(wgts, vals):
+        assert(len(wgts) == len(vals))
+        return list(zip(wgts, vals))
 
-    def zip_weights_values(self, weights, values):
-        assert(len(weights) == len(values))
-        return list(zip(weights, values))
-
-    def valid_weight(self, weight, target_weight):
-        return weight <= target_weight
-
-    def available_weight(self, weights_used, index):
+    def available_weight(self, candidate_weight, index):
         pass
-
 
 
 class KnapsackWithRepetition(Knapsack):
@@ -67,16 +58,18 @@ class KnapsackWithRepetition(Knapsack):
     def __init__(self, weights, values, capacity):
         super().__init__(weights, values, capacity)
 
-    def available_weight(self, weights_used, index):
+    def available_weight(self, candidate_weight, index):
         return True
+
 
 class KnapsackNoRepetition(Knapsack):
 
     def __init__(self, weights, values, capacity):
         super().__init__(weights, values, capacity)
 
-    def available_weight(self, weights_used, index):
-        return weights_used[index] == 0
+    def available_weight(self, candidate_weight, weight_index):
+        return self.weights_used[weight_index] == 0 and self.memo[candidate_weight].value != -1 and \
+               self.memo[candidate_weight].weights_used[weight_index] == 0
 
 
 if __name__ == '__main__':
@@ -85,5 +78,6 @@ if __name__ == '__main__':
     num_bars = int(match_object.group(2))
     weights = [int(w) for w in sys.stdin.readline().rstrip().split(" ")]
     knr = KnapsackNoRepetition(weights, weights, capacity)
-    print(knr.calc_max_value())
+    max_value = knr.calc_max_value()
+    print(max_value.value)
 
--- test_knapsack.py
+++ test_knapsack.py
@@ -1,29 +1,19 @@
-from knapsack import KnapsackWithRepetition, KnapsackNoRepetition, WeightsValue
+from knapsack import KnapsackWithRepetition, KnapsackNoRepetition, MemoItem
 import pytest
 
 @pytest.fixture
 def kswr_example():
     return KnapsackWithRepetition([6, 3, 4, 2], [30, 14, 16, 9], 10)
 
-@pytest.fixture
-def ksnr_example():
-    return KnapsackNoRepetition([6, 3, 4, 2], [30, 14, 16, 9], 10)
-
-def test_valid_weight(kswr_example):
-    assert(kswr_example.valid_weight(2, 2))
-
-
-def test_fetch_options(kswr_example):
-    assert(kswr_example.fetch_options(2, [0, 0, 0, 0]) == [(0, 9, 3)])
-
-
-def test__calc_max_value(kswr_example):
-    assert(kswr_example._calc_max_value(4, [0, 0, 0, 0]) == 18)
-
 
 def test_calc_max_value_kswr(kswr_example):
-    assert(kswr_example.calc_max_value() == 48)
+    assert(kswr_example.calc_max_value() == MemoItem(48, [1, 0, 0, 2], 4))
 
 
-def test_calc_max_value_ksnr(ksnr_example):
-    assert(ksnr_example.calc_max_value() == 46)
+@pytest.mark.parametrize(("weights", "values", "capacity", "exp_res"),[
+    ([6, 3, 4, 2], [30, 14, 16, 9], 10, MemoItem(46, [1, 0, 1, 0], 4)),
+    ([1, 4, 8], [1, 4, 8], 10, MemoItem(9, [1, 0, 1], 8)),
+])
+def test_calc_max_value_ksnr(weights, values, capacity, exp_res):
+    ksnr = KnapsackNoRepetition(weights, values, capacity)
+    assert(ksnr.calc_max_value() == exp_res)
